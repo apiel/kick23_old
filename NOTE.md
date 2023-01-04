97781c1c521ba755128ba9e3973c6e915b9dda21
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-01-04 21:54:48 +0100

    yo

--- NOTE.md
+++ NOTE.md
@@ -5,3 +5,9 @@ To fix noise issue due to pwm, maybe use small filter on output pin:
 - https://note.com/solder_state/n/ne33e32660d50
 - https://note.com/solder_state/n/n950ed088c3cb
 - https://note.com/solder_state/n/nca6a1dec3921
+
+I2S dac
+
+- MAX98357A
+
+https://dronebotworkshop.com/esp32-i2s/
--- linuxSDL/app.cpp
+++ linuxSDL/app.cpp
@@ -46,13 +46,21 @@ bool handleKeyboard(SDL_KeyboardEvent* event)
             buttonReleased();
         }
         break;
+    // case 20: {
+    //     if (event->type == SDL_KEYDOWN) {
+    //         printf("---------- buttonPressed\n");
+    //     } else {
+    //         printf("---------- buttonRelease\n");
+    //     }
+    //     break;
+    // }
     case 4: // A
         if (event->type == SDL_KEYDOWN) {
             rotaryChanged(-1);
         }
         break;
     case 22: // S
-    if (event->type == SDL_KEYDOWN) {
+        if (event->type == SDL_KEYDOWN) {
             rotaryPressed();
         }
         break;
@@ -66,170 +74,170 @@ bool handleKeyboard(SDL_KeyboardEvent* event)
         break;
     }
 
-    return true;
-}
-
-bool handleMouse(SDL_MouseButtonEvent* event)
-{
-    // SDL_Log("handleMouse %d %d %d %d\n", event->button, event->x, event->y, event->state);
-    SDL_Point mousePosition;
-    mousePosition.x = event->x;
-    mousePosition.y = event->y;
-
-    if (SDL_PointInRect(&mousePosition, &pot1)) {
-        uint8_t value = (float)(event->y - pot1.y) / (float)(pot1.h - pot1.y) * 100;
-        if (value > 100)
-            value = 100;
-        // APP_LOG("pot1 %d\n", value);
-        updatePot(0, value);
-    } else if (SDL_PointInRect(&mousePosition, &pot2)) {
-        uint8_t value = (float)(event->y - pot2.y) / (float)(pot2.h - pot2.y) * 100;
-        if (value > 100)
-            value = 100;
-        // APP_LOG("pot2 %d\n", value);
-        updatePot(1, value);
+        return true;
     }
-    return true;
-}
 
-bool mouseBtnDown = false;
-bool handleEvent()
-{
-    SDL_Event event;
+    bool handleMouse(SDL_MouseButtonEvent * event)
+    {
+        // SDL_Log("handleMouse %d %d %d %d\n", event->button, event->x, event->y, event->state);
+        SDL_Point mousePosition;
+        mousePosition.x = event->x;
+        mousePosition.y = event->y;
 
-    while (SDL_PollEvent(&event)) {
-        // if (event.type > 0x300 && event.type < 0x800) {
-        //     SDL_Log("handleEvent %d\n", event.type);
-        // }
-
-        switch (event.type) {
-        case SDL_QUIT:
-            return false;
-
-        case SDL_KEYUP:
-        case SDL_KEYDOWN:
-            return handleKeyboard(&event.key);
-
-        case SDL_MOUSEBUTTONDOWN: {
-            mouseBtnDown = true;
-            handleMouse(&event.button);
-            break;
+        if (SDL_PointInRect(&mousePosition, &pot1)) {
+            uint8_t value = (float)(event->y - pot1.y) / (float)(pot1.h - pot1.y) * 100;
+            if (value > 100)
+                value = 100;
+            // APP_LOG("pot1 %d\n", value);
+            updatePot(0, value);
+        } else if (SDL_PointInRect(&mousePosition, &pot2)) {
+            uint8_t value = (float)(event->y - pot2.y) / (float)(pot2.h - pot2.y) * 100;
+            if (value > 100)
+                value = 100;
+            // APP_LOG("pot2 %d\n", value);
+            updatePot(1, value);
         }
+        return true;
+    }
 
-        case SDL_MOUSEBUTTONUP: {
-            mouseBtnDown = false;
-            break;
-        }
+    bool mouseBtnDown = false;
+    bool handleEvent()
+    {
+        SDL_Event event;
 
-        case SDL_MOUSEMOTION: {
-            if (mouseBtnDown) {
+        while (SDL_PollEvent(&event)) {
+            // if (event.type > 0x300 && event.type < 0x800) {
+            //     SDL_Log("handleEvent %d\n", event.type);
+            // }
+
+            switch (event.type) {
+            case SDL_QUIT:
+                return false;
+
+            case SDL_KEYUP:
+            case SDL_KEYDOWN:
+                return handleKeyboard(&event.key);
+
+            case SDL_MOUSEBUTTONDOWN: {
+                mouseBtnDown = true;
                 handleMouse(&event.button);
+                break;
             }
-            break;
+
+            case SDL_MOUSEBUTTONUP: {
+                mouseBtnDown = false;
+                break;
+            }
+
+            case SDL_MOUSEMOTION: {
+                if (mouseBtnDown) {
+                    handleMouse(&event.button);
+                }
+                break;
+            }
+            }
         }
-        }
+
+        return true;
     }
 
-    return true;
-}
+    SDL_AudioDeviceID initAudio(SDL_AudioCallback callback)
+    {
+        SDL_AudioSpec spec, aspec;
 
-SDL_AudioDeviceID initAudio(SDL_AudioCallback callback)
-{
-    SDL_AudioSpec spec, aspec;
+        SDL_zero(spec);
+        spec.freq = SAMPLE_RATE;
+        spec.format = APP_AUDIO_FORMAT;
+        spec.channels = APP_CHANNELS;
+        spec.samples = APP_AUDIO_CHUNK;
+        spec.callback = callback;
+        spec.userdata = NULL;
 
-    SDL_zero(spec);
-    spec.freq = SAMPLE_RATE;
-    spec.format = APP_AUDIO_FORMAT;
-    spec.channels = APP_CHANNELS;
-    spec.samples = APP_AUDIO_CHUNK;
-    spec.callback = callback;
-    spec.userdata = NULL;
+        SDL_AudioDeviceID audioDevice = SDL_OpenAudioDevice(NULL, 0, &spec, &aspec, SDL_AUDIO_ALLOW_ANY_CHANGE);
+        if (audioDevice <= 0) {
+            fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+            return false;
+        }
 
-    SDL_AudioDeviceID audioDevice = SDL_OpenAudioDevice(NULL, 0, &spec, &aspec, SDL_AUDIO_ALLOW_ANY_CHANGE);
-    if (audioDevice <= 0) {
-        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
-        return false;
+        SDL_Log("aspec freq %d channel %d sample %d format %d", aspec.freq, aspec.channels, aspec.samples, aspec.format);
+
+        // Start playing, "unpause"
+        SDL_PauseAudioDevice(audioDevice, 0);
+        return audioDevice;
     }
 
-    SDL_Log("aspec freq %d channel %d sample %d format %d", aspec.freq, aspec.channels, aspec.samples, aspec.format);
+    void audioCallBack(void* userdata, Uint8* stream, int len)
+    {
+        static union sampleTUNT {
+            Uint8 ch[2];
+            int16_t sample;
+        } sampleDataU;
 
-    // Start playing, "unpause"
-    SDL_PauseAudioDevice(audioDevice, 0);
-    return audioDevice;
-}
-
-void audioCallBack(void* userdata, Uint8* stream, int len)
-{
-    static union sampleTUNT {
-        Uint8 ch[2];
-        int16_t sample;
-    } sampleDataU;
-
-    for (int i = 0; i < len; i++) {
-        sampleDataU.sample = (int16_t)(getSample() * 32767);
-        // printf("%d\n", sampleDataU.sample);
-        stream[i] = sampleDataU.ch[0];
-        i++;
-        stream[i] = sampleDataU.ch[1];
+        for (int i = 0; i < len; i++) {
+            sampleDataU.sample = (int16_t)(getSample() * 32767);
+            // printf("%d\n", sampleDataU.sample);
+            stream[i] = sampleDataU.ch[0];
+            i++;
+            stream[i] = sampleDataU.ch[1];
 
 #if APP_CHANNELS == 2
-        i++;
-        stream[i] = sampleDataU.ch[0];
-        i++;
-        stream[i] = sampleDataU.ch[1];
+            i++;
+            stream[i] = sampleDataU.ch[0];
+            i++;
+            stream[i] = sampleDataU.ch[1];
 #endif
-    }
-}
-
-int main(int argc, char* args[])
-{
-    SDL_Log(">>>>>>> Start APP\n");
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
-        return 1;
+        }
     }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "App",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        SCREEN_W, SCREEN_H,
-        SDL_WINDOW_SHOWN);
+    int main(int argc, char* args[])
+    {
+        SDL_Log(">>>>>>> Start APP\n");
 
-    if (window == NULL) {
-        fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+        if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+            fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+            return 1;
+        }
 
-    SDL_AudioDeviceID audioDevice = initAudio(audioCallBack);
-    if (SDL_getenv("APP_SKIP_AUDIO") == NULL && !audioDevice) {
-        return 1;
-    }
+        SDL_Window* window = SDL_CreateWindow(
+            "App",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_W, SCREEN_H,
+            SDL_WINDOW_SHOWN);
 
-    SDL_UpdateWindowSurface(window);
+        if (window == NULL) {
+            fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+            return 1;
+        }
+        SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
 
-    // uint8_t counter = 0;
-    while (handleEvent()) {
-        // if (counter < 3) {
-        //     printf("Trigger sound\n");
-        //     triggerSound();
-        //     SDL_Delay(1000);
-        //     counter++;
-        // }
-
-        Uint32 color = SDL_MapRGB(screenSurface->format, 255, 255, 255);
-
-        SDL_FillRect(screenSurface, &pot1, color);
-        SDL_FillRect(screenSurface, &pot2, color);
+        SDL_AudioDeviceID audioDevice = initAudio(audioCallBack);
+        if (SDL_getenv("APP_SKIP_AUDIO") == NULL && !audioDevice) {
+            return 1;
+        }
 
         SDL_UpdateWindowSurface(window);
-        // SDL_Delay(10);
+
+        // uint8_t counter = 0;
+        while (handleEvent()) {
+            // if (counter < 3) {
+            //     printf("Trigger sound\n");
+            //     triggerSound();
+            //     SDL_Delay(1000);
+            //     counter++;
+            // }
+
+            Uint32 color = SDL_MapRGB(screenSurface->format, 255, 255, 255);
+
+            SDL_FillRect(screenSurface, &pot1, color);
+            SDL_FillRect(screenSurface, &pot2, color);
+
+            SDL_UpdateWindowSurface(window);
+            // SDL_Delay(10);
+        }
+
+        SDL_DestroyWindow(window);
+        SDL_CloseAudioDevice(audioDevice);
+
+        SDL_Quit();
+        return 0;
     }
-
-    SDL_DestroyWindow(window);
-    SDL_CloseAudioDevice(audioDevice);
-
-    SDL_Quit();
-    return 0;
-}
